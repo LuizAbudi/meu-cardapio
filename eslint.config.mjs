d59441871e27ae608a3bcd6cd33a8ee10f8446b8
LuizAbudi <luizabudi@hotmail.com>
2025-02-10 19:53:31 -0300

    fix: ajusta formatação e estilo das configurações do ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,31 @@
-import { FlatCompat } from '@eslint/eslintrc';
-import eslintPluginImport from 'eslint-plugin-import';
-import eslintPluginPrettier from 'eslint-plugin-prettier';
+import { FlatCompat } from "@eslint/eslintrc";
+import eslintPluginImport from "eslint-plugin-import";
+import eslintPluginPrettier from "eslint-plugin-prettier";
 
 const compat = new FlatCompat();
 
 const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
+  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
   {
     plugins: {
       import: eslintPluginImport,
       prettier: eslintPluginPrettier,
     },
     rules: {
-      'prettier/prettier': 'error',
-      'sort-imports': 'off',
-      'import/order': [
-        'error',
+      "prettier/prettier": "error",
+      "sort-imports": "off",
+      "import/order": [
+        "error",
         {
-          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
-          'newlines-between': 'always',
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+          ],
+          "newlines-between": "always",
         },
       ],
     },
