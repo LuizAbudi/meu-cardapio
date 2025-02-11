7c6e31c75ad805af27eda65dabeb071981269e7f
LuizAbudi <luizabudi@hotmail.com>
2025-02-10 23:08:06 -0300

    feat: adiciona opcao de porcao inteira ou meia

--- src/components/menu-item-card.tsx
+++ src/components/menu-item-card.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import { Plus } from "lucide-react";
+import { useState } from "react";
 
 import { formatCurrency } from "@/utils/format";
 import { Button } from "@/components/ui/button";
@@ -33,6 +34,17 @@ interface MenuItemCardProps {
 
 export function MenuItemCard({ item, categoryName }: MenuItemCardProps) {
   const { addItem } = useCart();
+  const [selectedOption, setSelectedOption] = useState<"full" | "half">("full");
+
+  const handleAddToCart = () => {
+    const price =
+      selectedOption === "half"
+        ? item.halfPrice
+        : item.promotion?.inPromotion
+          ? item.promotion.price
+          : item.price;
+    addItem({ ...item, price });
+  };
 
   if (categoryName === "Porções") {
     return (
@@ -75,19 +87,52 @@ export function MenuItemCard({ item, categoryName }: MenuItemCardProps) {
         </CardHeader>
         <CardContent>
           <p className="text-sm text-muted-foreground">{item.description}</p>
+          <div className="mt-4 flex flex-row gap-3">
+            <div className="flex gap-10">
+              <div className="inline-flex items-center">
+                <label
+                  className="relative flex items-center cursor-pointer"
+                  htmlFor="html"
+                >
+                  <input
+                    name="full"
+                    type="radio"
+                    className="peer h-5 w-5 cursor-pointer appearance-none rounded-full border border-primary checked:border-primary transition-all"
+                    id="full"
+                    checked={selectedOption === "full"}
+                    onChange={() => setSelectedOption("full")}
+                  />
+                  <span className="absolute bg-primary w-3 h-3 rounded-full opacity-0 peer-checked:opacity-100 transition-opacity duration-200 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></span>
+                </label>
+                <label className="ml-2 cursor-pointer text-sm" htmlFor="full">
+                  Inteira
+                </label>
+              </div>
+
+              <div className="inline-flex items-center">
+                <label
+                  className="relative flex items-center cursor-pointer"
+                  htmlFor="half"
+                >
+                  <input
+                    name="half"
+                    type="radio"
+                    className="peer h-5 w-5 cursor-pointer appearance-none rounded-full border border-primary checked:border-primary transition-all"
+                    id="half"
+                    checked={selectedOption === "half"}
+                    onChange={() => setSelectedOption("half")}
+                  />
+                  <span className="absolute bg-primary w-3 h-3 rounded-full opacity-0 peer-checked:opacity-100 transition-opacity duration-200 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></span>
+                </label>
+                <label className="ml-2 cursor-pointer text-sm" htmlFor="half">
+                  Meia
+                </label>
+              </div>
+            </div>
+          </div>
         </CardContent>
         <CardFooter>
-          <Button
-            className="w-full"
-            onClick={() =>
-              addItem({
-                ...item,
-                price: item.promotion?.inPromotion
-                  ? item.promotion.price
-                  : item.price,
-              })
-            }
-          >
+          <Button className="w-full" onClick={handleAddToCart}>
             <Plus className="mr-2 h-4 w-4" />
             Adicionar ao Carrinho
           </Button>
